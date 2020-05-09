85204c4801aede127351c4d76fa02e9739af3754
stargarner <zinfandel0512@gmail.com>
2020-05-09 20:00:30 +0900

    update some sound

---
+++ wav2m4a.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Find;
+
+my($aacEncoder) = qw( C:/app/_console/neroAacEnc.exe );
+
+chdir 'sound-wav' or die "chdir failed. $!";
+for(<*.wav>){
+	my $aac = "../sound/$_";
+	$aac =~ s/\.wav$/.m4a/;
+	unlink $aac;
+	my $cmd = qq('$aacEncoder' -br 128000 -he -ignorelength -if '$_' -of '$aac');
+	print "$cmd\n";
+	system $cmd;
+}
